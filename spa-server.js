50634370688c648f84cb315ce8554e3f24c3d6f1
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2020-09-18 15:31:24 +0300

    version update

--- spa-server.js
+++ spa-server.js
@@ -11,6 +11,8 @@ const app = express();
 
 const getCliValue = key => (process.argv.filter(el => el.indexOf(key) === 0)[0] || '').substring(key.length);
 
+const fileExistsSync = (xpath) => fs.existsSync(xpath) && fs.lstatSync(xpath).isFile()
+
 const cliValueToProxyConfig = val => ({
   apiBase: val.substr(0, val.indexOf('::')),
   proxyBase: val.substring(val.indexOf('::') + 2),
@@ -29,10 +31,9 @@ const fullBasePath = path.join(process.cwd(), publicPath);
 console.log('[spa-server] public path:', fullBasePath);
 const fallback = getCliValue('--fallback=') || 'index.html';
 console.log('[spa-server] fallback used:', fallback);
-const fallbackExists = fs.existsSync(path.join(fullBasePath, fallback));
+const fallbackExists = fileExistsSync(path.join(fullBasePath, fallback));
 
-app.get('/:filePath', (req, res) => {
-  if(!fallbackExists){ return res.type('txt').send('Not found') }
+const sendDefaultFile = (req, res) => {
   const options = {
     root: fullBasePath,
     dotfiles: 'deny',
@@ -42,21 +43,29 @@ app.get('/:filePath', (req, res) => {
     },
   };
 
-  const fullPath = path.join(options.root, req.params.filePath);
-  const fileExists = fs.existsSync(fullPath);
+  const fullPath = path.join(options.root, req.params.filePath || '');
+  const fileExists = fileExistsSync(fullPath);
 
-  const fileName = fileExists ? req.params.filePath : 'index.html';
-  res.sendFile(fileName, options, (err) => {
+  if(!fileExists && !fallbackExists){
+    return res.type('txt').send('Not found X')
+  }
+
+  const fileName = fileExists ? req.params.filePath : fallback;
+  res.sendFile(fileName , options, (err) => {
     if (err) {
       next(err);
     } else {
       console.log('Sent:', fileName);
     }
   });
-});
+}
+
+app.get('/:filePath', sendDefaultFile);
+
+app.get('/', sendDefaultFile);
 
 app.use(function(req, res, next){
-  res.type('txt').send('Not found');
+  res.type('txt').send('Not found 404');
 });
 
 const port = getCliValue('--port=') || 3000;
